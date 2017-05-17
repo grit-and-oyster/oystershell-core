ef73fff86d5624c528e2d3c18e3fd79558d012f1
Andy Strange <code@grit-oyster.co.uk>
2017-05-17 18:48:51 +0100

    Added initial files

---
+++ oystershell-core.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* Plugin Name: 	Oystershell Core
+* Plugin URI:  	http://grit-oyster.co.uk/oystershell/
+* Description: 	The libraries, classes and functions that form the core of the Oystershell framework. Required by other Oystershell plugins.
+* Version: 		1.0
+* Author: 		Grit and Oyster Limited
+* Author URI: 	http://grit-oyster.co.uk/
+*/
+
+/**
+ * Core plugin file. Defines functions to load the various Oystershell Core elements.
+ *
+ * @link       http://grit-oyster.co.uk/
+ * @since      1.0.0
+ *
+ * @package    OSC_Core
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
